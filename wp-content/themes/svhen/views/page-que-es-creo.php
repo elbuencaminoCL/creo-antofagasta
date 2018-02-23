7ede6ab05645a729819acb06f0ec0f223a92fad7
svhen <sgarcia@svhen.com>
2018-02-23 15:50:09 -0500

    Creación página Consejo público

--- wp-content/themes/svhen/views/page-que-es-creo.php
+++ wp-content/themes/svhen/views/page-que-es-creo.php
@@ -35,19 +35,19 @@
 				<?php if ( have_rows( 'about-us_repeater' ) ) : ?>
 					<?php while ( have_rows( 'about-us_repeater' ) ) : the_row(); ?>
 
+						<?php $hex = get_sub_field('about-us_member_color');
+	  				list($r, $g, $b) = sscanf($hex, "#%02x%02x%02x"); ?>
+
 						<div class="col-xs-6 col-sm-3 col-md-2">
-							<figure class="row middle-xs memberImage-<?= $count; ?>">
+							<figure class="row middle-xs member-figure">
 								<img src="<?php the_sub_field( 'about-us_member_image' ); ?>" />
-								<div class="infoMember">
+								<div class="member-info" style="background-color: rgba( <?= "$r, $g, $b"; ?>, .8 );">
 									<p><?php the_sub_field( 'about-us_member_title' ); ?></p>
 									<p><?php the_sub_field( 'about-us_member_subtitle' ); ?></p>
 									<p><?php the_sub_field( 'about-us_member_desc' ); ?></p>
 								</div>
 							</figure>
 						</div>
-
-						<?php $hex = get_sub_field('about-us_member_color');
-	  				list($r1, $g1, $b1) = sscanf($hex, "#%02x%02x%02x"); ?>
 
 					<?php endwhile; ?>
 				<?php endif; ?>
@@ -69,20 +69,19 @@
 				<?php if ( have_rows( 'experts_repeater' ) ) : ?>
 					<?php while ( have_rows( 'experts_repeater' ) ) : the_row(); ?>
 
-						<div class="col-xs-6 col-md-3 col-parent">
+						<?php $hex = get_sub_field('experts_member_color');
+	  				list($r, $g, $b) = sscanf($hex, "#%02x%02x%02x"); ?>
+
+						<div class="col-xs-6 col-md-3">
 							<figure class="row middle-xs expert-figure">
 								<img src="<?php the_sub_field( 'experts_member_image' ); ?>" />
-								<div class="infoExpert">
+								<div class="expert-info" style="background-color: rgba( <?= "$r, $g, $b"; ?>, .8 );">
+									<p><?php the_sub_field( 'experts_member_desc' ); ?></p>
 									<p><?php the_sub_field( 'experts_member_title' ); ?></p>
 									<p><?php the_sub_field( 'experts_member_subtitle' ); ?></p>
-									<p><?php the_sub_field( 'experts_member_desc' ); ?></p>
-									<p><?php $hex = the_sub_field('experts_member_color'); list($r, $g, $b) = sscanf($hex, "#%02x%02x%02x"); ?></p>
 								</div>
 							</figure>
 						</div>
-
-						<?php $hex = get_sub_field('experts_member_color');
-	  				list($r, $g, $b) = sscanf($hex, "#%02x%02x%02x"); ?>
 
 					<?php endwhile; ?>
 				<?php endif; ?>
@@ -93,20 +92,6 @@
 </div>
 
 
-<style type="text/css">
 
-	.infoExpert:hover {
-
-	}
-
-	.col-parent .infoExpert:nth-child(even) {
-	  background-color: red;
-	}
-
-/* 	.col-parent .infoExpert:hover:nth-child(even) {
-  background-color: rgba( <?= "$r, $g, $b"; ?>, .8 );
-} */
-
-</style>
 
 <?php get_footer(); ?>
