aab0f7461189d2b33f85128b0d152bb0192027c9
svhen <sgarcia@svhen.com>
2018-03-01 17:52:06 -0500

    Plantilla proyectos: Estilos

--- wp-content/plugins/advanced-custom-fields-pro/includes/fields/class-acf-field-range.php
+++ wp-content/plugins/advanced-custom-fields-pro/includes/fields/class-acf-field-range.php
@@ -3,8 +3,8 @@
 if( ! class_exists('acf_field_range') ) :
 
 class acf_field_range extends acf_field_number {
-	
-	
+
+
 	/*
 	*  initialize
 	*
@@ -17,9 +17,9 @@ class acf_field_range extends acf_field_number {
 	*  @param	n/a
 	*  @return	n/a
 	*/
-	
+
 	function initialize() {
-		
+
 		// vars
 		$this->name = 'range';
 		$this->label = __("Range",'acf');
@@ -31,10 +31,10 @@ class acf_field_range extends acf_field_number {
 			'prepend'		=> '',
 			'append'		=> ''
 		);
-		
+
 	}
-		
-	
+
+
 	/*
 	*  render_field()
 	*
@@ -46,93 +46,90 @@ class acf_field_range extends acf_field_number {
 	*  @since	3.6
 	*  @date	23/01/13
 	*/
-	
+
 	function render_field( $field ) {
-		
+
 		// vars
 		$atts = array();
 		$keys = array( 'type', 'id', 'class', 'name', 'value', 'min', 'max', 'step' );
 		$keys2 = array( 'readonly', 'disabled', 'required' );
 		$html = '';
-		
-		
+
+
 		// step
 		if( !$field['step'] ) $field['step'] = 1;
-		
-		
+
+
 		// min / max
 		if( !$field['min'] ) $field['min'] = 0;
 		if( !$field['max'] ) $field['max'] = 100;
-		
-		
+
+
 		// value
 		if( !is_numeric($field['value']) ) {
 			$field['value'] = 0;
 		}
-		
-		
+
+
 		// atts (value="123")
 		foreach( $keys as $k ) {
 			if( isset($field[ $k ]) ) $atts[ $k ] = $field[ $k ];
 		}
-		
-		
+
+
 		// atts2 (disabled="disabled")
 		foreach( $keys2 as $k ) {
 			if( !empty($field[ $k ]) ) $atts[ $k ] = $k;
 		}
-		
-		
+
+
 		// remove empty atts
 		$atts = acf_clean_atts( $atts );
-		
-		
+
+
 		// open
 		$html .= '<div class="acf-range-wrap">';
-			
-			
+
+
 			// prepend
 			if( $field['prepend'] !== '' ) {
 				$html .= '<div class="acf-prepend">' . acf_esc_html($field['prepend']) . '</div>';
 			}
-			
-			
+
+
 			// range
 			$html .= acf_get_text_input( $atts );
-			
-			
+
+
 			// input
 			$len = strlen( (string) $field['max'] );
 			$html .= acf_get_text_input(array(
-				'type'	=> 'number', 
-				'id'	=> $atts['id'] . '-alt', 
+				'type'	=> 'number',
+				'id'	=> $atts['id'] . '-alt',
 				'value'	=> $atts['value'],
 				'step'	=> $atts['step'],
-<<<<<<< HEAD
 				'min'	=> $atts['min'],
 				'max'	=> $atts['max'],
-=======
->>>>>>> 98eca4b5a44fb3ef73e63f8181b42e7c76afc0a6
 				'style'	=> 'width: ' . (1.8 + $len*0.7) . 'em;'
 			));
-			
-			
+
+
 			// append
 			if( $field['append'] !== '' ) {
 				$html .= '<div class="acf-append">' . acf_esc_html($field['append']) . '</div>';
 			}
-		
-		
+
+
 		// close
 		$html .= '</div>';
-		
-		
+
+
 		// return
 		echo $html;
-		
+
 	}
-	
-	
+
+
 	/*
 	*  render_field_settings()
 	*
@@ -145,9 +142,9 @@ class acf_field_range extends acf_field_number {
 	*
 	*  @param	$field	- an array holding all the field's data
 	*/
-	
+
 	function render_field_settings( $field ) {
-		
+
 		// default_value
 		acf_render_field_setting( $field, array(
 			'label'			=> __('Default Value','acf'),
@@ -155,8 +152,8 @@ class acf_field_range extends acf_field_number {
 			'type'			=> 'number',
 			'name'			=> 'default_value',
 		));
-		
-		
+
+
 		// min
 		acf_render_field_setting( $field, array(
 			'label'			=> __('Minimum Value','acf'),
@@ -165,8 +162,8 @@ class acf_field_range extends acf_field_number {
 			'name'			=> 'min',
 			'placeholder'	=> '0'
 		));
-		
-		
+
+
 		// max
 		acf_render_field_setting( $field, array(
 			'label'			=> __('Maximum Value','acf'),
@@ -175,8 +172,8 @@ class acf_field_range extends acf_field_number {
 			'name'			=> 'max',
 			'placeholder'	=> '100'
 		));
-		
-		
+
+
 		// step
 		acf_render_field_setting( $field, array(
 			'label'			=> __('Step Size','acf'),
@@ -185,8 +182,8 @@ class acf_field_range extends acf_field_number {
 			'name'			=> 'step',
 			'placeholder'	=> '1'
 		));
-		
-		
+
+
 		// prepend
 		acf_render_field_setting( $field, array(
 			'label'			=> __('Prepend','acf'),
@@ -194,8 +191,8 @@ class acf_field_range extends acf_field_number {
 			'type'			=> 'text',
 			'name'			=> 'prepend',
 		));
-		
-		
+
+
 		// append
 		acf_render_field_setting( $field, array(
 			'label'			=> __('Append','acf'),
@@ -203,10 +200,10 @@ class acf_field_range extends acf_field_number {
 			'type'			=> 'text',
 			'name'			=> 'append',
 		));
-		
+
 	}
-	
-	
+
+
 }
 
 
--- wp-content/plugins/advanced-custom-fields-pro/includes/locations/class-acf-location-post-type.php
+++ wp-content/plugins/advanced-custom-fields-pro/includes/locations/class-acf-location-post-type.php
@@ -1,12 +1,12 @@
-<?php 
+<?php
 
 if( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 if( ! class_exists('acf_location_post_type') ) :
 
 class acf_location_post_type extends acf_location {
-	
-	
+
+
 	/*
 	*  __construct
 	*
@@ -19,17 +19,17 @@ class acf_location_post_type extends acf_location {
 	*  @param	n/a
 	*  @return	n/a
 	*/
-	
+
 	function initialize() {
-		
+
 		// vars
 		$this->name = 'post_type';
 		$this->label = __("Post Type",'acf');
 		$this->category = 'post';
-    	
+
 	}
-	
-	
+
+
 	/*
 	*  get_post_type
 	*
@@ -42,28 +42,28 @@ class acf_location_post_type extends acf_location {
 	*  @param	$options (int)
 	*  @return	(mixed)
 	*/
-	
+
 	function get_post_type( $screen ) {
-		
+
 		// vars
 		$post_id = acf_maybe_get( $screen, 'post_id' );
 		$post_type = acf_maybe_get( $screen, 'post_type' );
-		
-		
+
+
 		// post_type
 		if( $post_type ) return $post_type;
-		
-		
+
+
 		// $post_id
 		if( $post_id ) return get_post_type( $post_id );
-		
-		
+
+
 		// return
 		return false;
-		
+
 	}
-	
-	
+
+
 	/*
 	*  rule_match
 	*
@@ -73,27 +73,27 @@ class acf_location_post_type extends acf_location {
 	*  @date	3/01/13
 	*  @since	3.5.7
 	*
-	*  @param	$match (boolean) 
+	*  @param	$match (boolean)
 	*  @param	$rule (array)
 	*  @return	$options (array)
 	*/
-	
+
 	function rule_match( $result, $rule, $screen ) {
-		
+
 		// vars
 		$post_type = $this->get_post_type( $screen );
-		
-		
+
+
 		// bail early if no post_type found (not a post)
 		if( !$post_type ) return false;
-		
-		
+
+
 		// match
 		return $this->compare( $post_type, $rule );
-		
+
 	}
-	
-	
+
+
 	/*
 	*  rule_operators
 	*
@@ -106,26 +106,22 @@ class acf_location_post_type extends acf_location {
 	*  @param	n/a
 	*  @return	(array)
 	*/
-	
+
 	function rule_values( $choices, $rule ) {
-		
+
 		// get post types
 		// - removed show_ui to allow 3rd party code to register a post type using a custom admin edit page
 		$post_types = acf_get_post_types(array(
-<<<<<<< HEAD
-			'show_ui'	=> 1, 
-=======
-			//'show_ui'	=> 1, 
->>>>>>> 98eca4b5a44fb3ef73e63f8181b42e7c76afc0a6
+			'show_ui'	=> 1,
 			'exclude'	=> array('attachment')
 		));
-		
-		
+
+
 		// return choices
 		return acf_get_pretty_post_types( $post_types );
-		
+
 	}
-	
+
 }
 
 // initialize
