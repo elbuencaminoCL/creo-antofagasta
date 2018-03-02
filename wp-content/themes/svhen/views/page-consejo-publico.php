737f566d3b4a87de9fdde344cc81d70beddf8585
svhen <sgarcia@svhen.com>
2018-03-02 12:38:47 -0500

    Solucionar Conflictos

--- wp-content/themes/svhen/views/page-consejo-publico.php
+++ wp-content/themes/svhen/views/page-consejo-publico.php
@@ -7,7 +7,7 @@
 
 ?>
 
-<section class="container">
+<section>
 	<figure class="row middle-xs center-xs">
 		<img src="<?php the_field( 'committe_banner_image' ); ?>" class="believe-banner-image">
 		<div class="banner-content container">
@@ -15,7 +15,7 @@
 		</div>
 	</figure>
 
-	<div class="row-xs-1 row-sm-1 row-md-1 bg-color-mintcream pd-30">
+	<div class="row-xs-1 row-sm-1 row-md-1 bg-mintcream pd-30">
 		<div class="col-xs-10 col-sm-8 col-md-6 mg-0-auto-i">
 			<p><?php the_field( 'committe_banner_desc' ); ?></p>
 		</div>
@@ -23,60 +23,61 @@
 </section>
 
 
-<section class="container bg-color-azure pd-30">
-	<div class="center-xs space-bottom">
-		<h3><?php the_field( 'committe_title' ); ?></h3>
-		<p><?php the_field( 'committe_desc' ); ?></p>
-	</div>
+<section class="bg-azure pd-30">
+	<div class="container">
+		<div class="center-xs space-bottom">
+			<h3><?php the_field( 'committe_title' ); ?></h3>
+			<p><?php the_field( 'committe_desc' ); ?></p>
+		</div>
 
-	<div class="bg-color-mintcream pd-30">
-		<div class="row row-xs-1 row-md-2">
-			<?php if ( have_rows( 'committe_main_repeater' ) ) : ?>
-				<?php while ( have_rows( 'committe_main_repeater' ) ) : the_row(); ?>
-					<?php $hex = get_sub_field('integrant_main_color');
-  				list($r, $g, $b) = sscanf($hex, "#%02x%02x%02x"); ?>
-					<div class="col-xs-12  col-md-6 space-bottom">
-						<div class="row row-xs-2 no-gutter">
-							<div class="col-xs-4">
-								<figure class="row middle-xs member-figure">
-									<img src="<?php the_sub_field( 'integrant_main_image' ); ?>" />
-								</figure>
-							</div>
+		<div class="bg-mintcream pd-30">
+			<div class="row row-xs-1 row-md-2">
+				<?php if ( have_rows( 'committe_main_repeater' ) ) : ?>
+					<?php while ( have_rows( 'committe_main_repeater' ) ) : the_row(); ?>
+						<?php $hex = get_sub_field('integrant_main_color');
+	  				list($r, $g, $b) = sscanf($hex, "#%02x%02x%02x"); ?>
+						<div class="col-xs-12  col-md-6 space-bottom">
+							<div class="row row-xs-2 no-gutter">
+								<div class="col-xs-4">
+									<figure class="row middle-xs member-figure">
+										<img src="<?php the_sub_field( 'integrant_main_image' ); ?>" />
+									</figure>
+								</div>
 
-							<div class="col-xs-8 f-white pd-15" style="background-color: rgba( <?= "$r, $g, $b"; ?>, .8 );">
-								<p><?php the_sub_field( 'integrant_main_desc' ); ?></p>
-								<p class="f-bigger"><?php the_sub_field( 'integrant_main_title' ); ?></p>
-								<p><?php the_sub_field( 'integrant_main_subtitle' ); ?></p>
+								<div class="col-xs-8 f-white pd-15" style="background-color: rgba( <?= "$r, $g, $b"; ?>, .8 );">
+									<p><?php the_sub_field( 'integrant_main_desc' ); ?></p>
+									<p class="f-bigger"><?php the_sub_field( 'integrant_main_title' ); ?></p>
+									<p><?php the_sub_field( 'integrant_main_subtitle' ); ?></p>
+								</div>
 							</div>
 						</div>
-					</div>
-				<?php endwhile; ?>
-			<?php endif; ?>
-		</div>
+					<?php endwhile; ?>
+				<?php endif; ?>
+			</div>
 
-		<div class="row row-xs-2 row-md-4">
-			<?php if ( have_rows( 'committe_repeater' ) ) : ?>
-				<?php while ( have_rows( 'committe_repeater' ) ) : the_row(); ?>
+			<div class="row row-xs-2 row-md-4">
+				<?php if ( have_rows( 'committe_repeater' ) ) : ?>
+					<?php while ( have_rows( 'committe_repeater' ) ) : the_row(); ?>
 
-					<?php $hex = get_sub_field('integrant_color');
-  				list($r, $g, $b) = sscanf($hex, "#%02x%02x%02x"); ?>
+						<?php $hex = get_sub_field('integrant_color');
+	  				list($r, $g, $b) = sscanf($hex, "#%02x%02x%02x"); ?>
 
-					<div class="col-xs-6 col-md-3 space-bottom">
-						<figure class="row middle-xs expert-figure">
-							<img src="<?php the_sub_field( 'integrant_image' ); ?>" />
-							<div class="expert-info" style="background-color: rgba( <?= "$r, $g, $b"; ?>, .8 );">
-								<p><?php the_sub_field( 'integrant_desc' ); ?></p>
-								<p><?php the_sub_field( 'integrant_title' ); ?></p>
-								<p><?php the_sub_field( 'integrant_subtitle' ); ?></p>
-							</div>
-						</figure>
-					</div>
+						<div class="col-xs-6 col-md-3 space-bottom">
+							<figure class="row middle-xs expert-figure">
+								<img src="<?php the_sub_field( 'integrant_image' ); ?>" />
+								<div class="expert-info" style="background-color: rgba( <?= "$r, $g, $b"; ?>, .8 );">
+									<p><?php the_sub_field( 'integrant_desc' ); ?></p>
+									<p><?php the_sub_field( 'integrant_title' ); ?></p>
+									<p><?php the_sub_field( 'integrant_subtitle' ); ?></p>
+								</div>
+							</figure>
+						</div>
 
-				<?php endwhile; ?>
-			<?php endif; ?>
+					<?php endwhile; ?>
+				<?php endif; ?>
+			</div>
 		</div>
 	</div>
-
 </section>
 
 
