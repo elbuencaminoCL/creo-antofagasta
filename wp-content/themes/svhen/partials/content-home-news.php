ca83deba12378a1d728736c4dd81b7c6a4b8d629
Jair Garcia <jgarcia@svhen.com>
2018-03-02 20:50:45 -0500

    News style responsive

--- wp-content/themes/svhen/partials/content-home-news.php
+++ wp-content/themes/svhen/partials/content-home-news.php
@@ -33,29 +33,29 @@
 					        <div class="card card--news card--news--home" style="border-left: 6px solid <?= get_field('color_picker'); ?>;">
 					        	<div class="card--news--home_container">
 											<?php if ( get_the_post_thumbnail() ) : ?>
-												<figure class="card--news--home_figure hide-xs show-md">
-													<?php the_post_thumbnail(); ?>
-												</figure>
+											<figure class="card--news--home_figure hide-xs show-md">
+												<?php the_post_thumbnail(); ?>
+											</figure>
 											<?php endif; ?>
 												<?php
 										      $hex = get_field('color_picker');
 										      list($r, $g, $b) = sscanf($hex, "#%02x%02x%02x");
 										    ?>
 
-												<div class="card--news--home_content" style="background-color: rgba( <?= "$r, $g, $b"; ?>, .8 );">
-													<p class="entry-meta f-small hide-md"><?php the_time("j"); ?> de <?php the_time("F Y"); ?></p>
-													<div class="hide-xs show-md">
-														<div class="card--news--home_category f-small">
-															<?= the_category( ' - ' ); ?>
-														</div>
+											<div class="card--news--home_content" style="background-color: rgba( <?= "$r, $g, $b"; ?>, .8 );">
+												<p class="entry-meta f-small hide-md"><?php the_time("j"); ?> de <?php the_time("F Y"); ?></p>
+												<div class="hide-xs show-md">
+													<div class="card--news--home_category f-small">
+														<?= the_category( ' - ' ); ?>
 													</div>
-													<h4 class="card--news--home_title">
-														<a href="<?php the_permalink(); ?>"><?= wp_html_excerpt( get_the_title(), 100, '...' ); ?></a>
-													</h4>
-													<div class="card--news--home_caption hide-xs show-md"><?= wp_html_excerpt( get_the_content(), 115, '...' ); ?></div>
-													<p class="f-small"><a href="<?php the_permalink(); ?>">Leer más ></a></p>
 												</div>
+												<h4 class="card--news--home_title">
+													<a href="<?php the_permalink(); ?>"><?= wp_html_excerpt( get_the_title(), 100, '...' ); ?></a>
+												</h4>
+												<div class="card--news--home_caption hide-xs show-md"><?= wp_html_excerpt( get_the_content(), 115, '...' ); ?></div>
+												<p class="f-small"><a href="<?php the_permalink(); ?>">Leer más ></a></p>
 											</div>
+										</div>
 									</div>
 								</div>
 							<?php endif; ?>
--- wp-content/themes/svhen/views/page-noticias.php
+++ wp-content/themes/svhen/views/page-noticias.php
@@ -6,10 +6,13 @@
   get_header();
 ?>
 
-<section>
+
+<section class="section">
 	<div class="container">
 		<h1>Noticias</h1>
-		<?= do_shortcode( '[ajax_load_more container_type="div" post_type="post" posts_per_page="4" scroll_distance="100"  transition_container_classes="row row-xs-1 row-sm-2 row-md-4" transition="slide" transition_speed="300"]' ); ?>
+		<div class="card--news--home_normal">
+			<?= do_shortcode( '[ajax_load_more container_type="div" post_type="post" posts_per_page="8" scroll_distance="100"  transition_container_classes="row row-xs-1 row-md-4" transition="slide" transition_speed="300"]' ); ?>
+		</div>
 	</div>
 </section>
 
